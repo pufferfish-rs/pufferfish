6fa3e9651595422b93473e19024ddaa34302cd1f
MarRZ <junmo.moon8@gmail.com>
2022-04-29 21:02:11 -0400

    Poorly implement custom state and callbacks

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,8 +1,17 @@
 use pufferfish::App;
 
+struct State {
+    x: u64,
+}
+
 fn main() {
     App::new()
         .with_title("Hello World")
         .with_size(1024, 768)
+        .add_state(State { x: 0 })
+        .add_callback(|state: &mut State| {
+            state.x += 1;
+            println!("{}", state.x);
+        })
         .run();
 }
--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,27 @@
+use std::any::{TypeId, Any};
 use std::borrow::Cow;
+use std::collections::HashMap;
 
 use fugu::Context;
 use sdl2::event::Event;
 use sdl2::video::GLProfile;
 
+pub trait Callback<Args> {
+    fn call(&self, args: &mut HashMap<TypeId, Box<dyn Any>>);
+}
+
+impl<A: 'static, F> Callback<(A,)> for F where F: Fn(&mut A) {
+    fn call(&self, args: &mut HashMap<TypeId, Box<dyn Any>>) {
+        let a = args.get_mut(&TypeId::of::<A>()).unwrap().downcast_mut().unwrap();
+        self(a)
+    }
+}
+
 pub struct App {
     title: Cow<'static, str>,
     size: (u32, u32),
+    state: HashMap<TypeId, Box<dyn Any>>,
+    callbacks: Option<Box<dyn Fn(&mut HashMap<TypeId, Box<dyn Any>>)>>,
 }
 
 impl App {
@@ -14,6 +29,8 @@ impl App {
         App {
             title: "Pufferfish".into(),
             size: (800, 600),
+            state: HashMap::new(),
+            callbacks: Some(Box::new(|_| {})),
         }
     }
 
@@ -27,7 +44,21 @@ impl App {
         self
     }
 
-    pub fn run(self) {
+    pub fn add_state<T: 'static>(mut self, state: T) -> App {
+        self.state.insert(TypeId::of::<T>(), Box::new(state));
+        self
+    }
+
+    pub fn add_callback<Args, T: Callback<Args> + 'static>(mut self, callback: T) -> App {
+        let cbs = self.callbacks.take().unwrap();
+        self.callbacks = Some(Box::new(move |args: &mut _| {
+            cbs(args);
+            callback.call(args);
+        }));
+        self
+    }
+
+    pub fn run(mut self) {
         let sdl_context = sdl2::init().unwrap();
         let video_subsystem = sdl_context.video().unwrap();
 
@@ -54,6 +85,8 @@ impl App {
                     _ => {}
                 }
             }
+
+            (self.callbacks.as_ref().unwrap())(&mut self.state);
 
             window.gl_swap_window();
         }
